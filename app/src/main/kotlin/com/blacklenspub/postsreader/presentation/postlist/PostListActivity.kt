b05d775c7b7c8acdc5263f866e7e9499f1ac3b60
Dew Parin <parinsura@gmail.com>
2017-06-07 18:44:54 +0700

    Replace MainActivity by PostListActivity.

---
+++ app/src/main/kotlin/com/blacklenspub/postsreader/presentation/postlist/PostListActivity.kt
@@ -0,0 +1,13 @@
+package com.blacklenspub.postsreader.presentation.postlist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.blacklenspub.postsreader.R
+
+class PostListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_list)
+    }
+}
